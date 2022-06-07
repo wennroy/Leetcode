4830658aed1148027abc452ebe2b06ca5583725d
wennroy <wennroy@gmail.com>
2022-06-07 16:12:43 -0500

    向量取代斜率

--- 数学/1037valid-boomerang.py
+++ 数学/1037valid-boomerang.py
@@ -17,3 +17,15 @@ class Solution:
         else:
             return True
 
+class Solution:
+    def isBoomerang(self, points: List[List[int]]) -> bool:
+        v1 = (points[1][0] - points[0][0], points[1][1] - points[0][1])
+        v2 = (points[2][0] - points[0][0], points[2][1] - points[0][1])
+        return v1[0] * v2[1] - v1[1] * v2[0] != 0
+
+'''
+作者：LeetCode-Solution
+链接：https://leetcode.cn/problems/valid-boomerang/solution/you-xiao-de-hui-xuan-biao-by-leetcode-so-yqby/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
