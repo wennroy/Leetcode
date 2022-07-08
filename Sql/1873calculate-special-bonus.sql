c8eaf05116fb4e7ae60fb4cd02f5a7a728a5ee39
wennroy <wennroy@gmail.com>
2022-07-08 02:27:16 -0500

    CASE WHEN exp THEN col ELSE xxx 和 IF(CONDITION, TRUE_VALUE, FALSE_VALUE)

---
+++ Sql/1873calculate-special-bonus.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+
+# CASE WHEN exp THEN col ELSE xxx
+# REGEXP
+select employee_id, case when employee_id % 2 = 1 and name not regexp('^M') then salary else 0 end as bonus
+from Employees
+;
+
+# IF(CONDITION, TRUE_VALUE, FALSE_VALUE)
+# LEFT(x,a)
+select employee_id, IF(employee_id % 2 != 0 and left(name,1) != 'M',salary,0) as bonus
+from Employees
+order by employee_id
