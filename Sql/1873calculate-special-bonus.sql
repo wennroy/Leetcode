ddf5b12155d0e70d17390c91a819e1b0dcec6fc5
wennroy <wennroy@gmail.com>
2022-07-08 02:28:42 -0500

    CASE WHEN exp THEN col ELSE xxx 和 IF(CONDITION, TRUE_VALUE, FALSE_VALUE)

--- Sql/1873calculate-special-bonus.sql
+++ Sql/1873calculate-special-bonus.sql
@@ -4,6 +4,7 @@
 # REGEXP
 select employee_id, case when employee_id % 2 = 1 and name not regexp('^M') then salary else 0 end as bonus
 from Employees
+ORDER BY employee_id
 ;
 
 # IF(CONDITION, TRUE_VALUE, FALSE_VALUE)
@@ -11,3 +12,4 @@ from Employees
 select employee_id, IF(employee_id % 2 != 0 and left(name,1) != 'M',salary,0) as bonus
 from Employees
 order by employee_id
+;
