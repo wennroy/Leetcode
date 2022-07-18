02d74ac4433d54d5fccfe0317c7ec08a10b64a4e
wennroy <wennroy@gmail.com>
2022-07-18 17:30:46 -0500

    2022-07-18 min_stack

---
+++ 栈模拟/155min-stack.py
@@ -0,0 +1,20 @@
+# 看了解答才看懂了题目= =
+
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = [math.inf]
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        self.min_stack.append(min(x, self.min_stack[-1]))
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def min(self) -> int:
+        return self.min_stack[-1]
