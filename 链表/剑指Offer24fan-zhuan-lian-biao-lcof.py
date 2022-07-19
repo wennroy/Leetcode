2c0a4b902b2df3caa8b472131d24fe29d6b007b5
wennroy <wennroy@gmail.com>
2022-07-18 21:07:02 -0500

    2022-07-18 链表翻转，遍历两遍。或者递归

---
+++ 链表/剑指Offer24fan-zhuan-lian-biao-lcof.py
@@ -0,0 +1,54 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        pt = head
+        stack = []
+        while pt:
+            stack.append(pt.val)
+            pt = pt.next
+        pt = head
+        while pt:
+            pt.val = stack.pop()
+            pt = pt.next
+        return head
+
+
+'''
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        newHead = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return newHead
+
+
+# 作者：LeetCode-Solution
+# 链接：https://leetcode.cn/problems/fan-zhuan-lian-biao-lcof/solution/fan-zhuan-lian-biao-by-leetcode-solution-jvs5/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
