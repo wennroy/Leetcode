f6559b39b6141016c3c752d2375424f961eb23ef
wennroy <wennroy@gmail.com>
2022-07-20 14:52:44 -0500

    2022-07-20 SELECT语句可放入运算中进行简化， ROUND(x, 2) * 100 仍然保留两位小数，但会在开始时四舍五入，33.00 vs 33.33

--- Sql/sql基础/1633percentage-of-users-attended-a-contest.sql
+++ Sql/sql基础/1633percentage-of-users-attended-a-contest.sql
@@ -8,3 +8,13 @@ ORDER BY percentage DESC, contest_id ASC;
 # ROUND(x * 100, 2) 正常保留两位小数
 
 # 33.00 和 33.33
+
+select contest_id,round(count(user_id) / (select count(1) from Users) * 100, 2) as percentage
+from Register
+group by contest_id
+order by percentage desc,contest_id
+
+# 作者：erke
+# 链接：https://leetcode.cn/problems/percentage-of-users-attended-a-contest/solution/roundcountuser_id-select-count1-from-use-amzh/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
