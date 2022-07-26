3f1d8c52cf3958f246fbfd517b44177874b8cb80
wennroy <wennroy@gmail.com>
2022-07-25 23:27:49 -0500

    2022 07-25 三种方法解决匹配子树，现在只会两种，哈希树不会~

---
+++ 二叉树/572subtree-of-another-tree.py
@@ -0,0 +1,217 @@
+# 简单做法：深度搜索+暴力枚举匹配
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: TreeNode, subRoot: TreeNode) -> bool:
+        def isequal(root1: TreeNode, root2: TreeNode) -> bool:
+            if not root1 and not root2:
+                return True
+            elif not root1 or not root2:
+                return False
+
+            if root1.val != root2.val:
+                return False
+            ans1, ans2 = isequal(root1.left, root2.left), isequal(root1.right, root2.right)
+            if ans1 and ans2:
+                return True
+            else:
+                return False
+
+        stack = [root]
+        while stack:
+            pt = stack.pop()
+            res = isequal(pt, subRoot)
+            if res:
+                return True
+            if pt.left:
+                stack.append(pt.left)
+            if pt.right:
+                stack.append(pt.right)
+
+        return False
+
+# 中等做法：引入lNull和rNull两个空值的DFS串匹配。在判断「ss 的深度优先搜索序列包含 tt 的深度优先搜索序列」的时候，可以暴力匹配，也可以使用 \text{KMP}KMP 或者 \text{Rabin-Karp}Rabin-Karp 算法
+
+# 这里给出用 KMP 判断的代码实现。
+'''
+class Solution {
+    List<Integer> sOrder = new ArrayList<Integer>();
+    List<Integer> tOrder = new ArrayList<Integer>();
+    int maxElement, lNull, rNull;
+
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        maxElement = Integer.MIN_VALUE;
+        getMaxElement(s);
+        getMaxElement(t);
+        lNull = maxElement + 1;
+        rNull = maxElement + 2;
+
+        getDfsOrder(s, sOrder);
+        getDfsOrder(t, tOrder);
+
+        return kmp();
+    }
+
+    public void getMaxElement(TreeNode t) {
+        if (t == null) {
+            return;
+        }
+        maxElement = Math.max(maxElement, t.val);
+        getMaxElement(t.left);
+        getMaxElement(t.right);
+    }
+
+    public void getDfsOrder(TreeNode t, List<Integer> tar) {
+        if (t == null) {
+            return;
+        }
+        tar.add(t.val);
+        if (t.left != null) {
+            getDfsOrder(t.left, tar);
+        } else {
+            tar.add(lNull);
+        }
+        if (t.right != null) {
+            getDfsOrder(t.right, tar);
+        } else {
+            tar.add(rNull);
+        }
+    }
+
+    public boolean kmp() {
+        int sLen = sOrder.size(), tLen = tOrder.size();
+        int[] fail = new int[tOrder.size()];
+        Arrays.fill(fail, -1);
+        for (int i = 1, j = -1; i < tLen; ++i) {
+            while (j != -1 && !(tOrder.get(i).equals(tOrder.get(j + 1)))) {
+                j = fail[j];
+            }
+            if (tOrder.get(i).equals(tOrder.get(j + 1))) {
+                ++j;
+            }
+            fail[i] = j;
+        }
+        for (int i = 0, j = -1; i < sLen; ++i) {
+            while (j != -1 && !(sOrder.get(i).equals(tOrder.get(j + 1)))) {
+                j = fail[j];
+            }
+            if (sOrder.get(i).equals(tOrder.get(j + 1))) {
+                ++j;
+            }
+            if (j == tLen - 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+作者：LeetCode-Solution
+链接：https://leetcode.cn/problems/subtree-of-another-tree/solution/ling-yi-ge-shu-de-zi-shu-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
+
+
+# 取巧做法：二叉树序列化
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+
+    def isSubtree(self, s, t):
+        """
+        :type s: TreeNode
+        :type t: TreeNode
+        :rtype: bool
+        """
+
+        def ser(root):
+            if not root:    return '#'
+            st = ' ' + str(ser(root.left)) + ' ' + str(ser(root.right)) + ' ' + str(
+                root.val) + ' '  # 前后加空格，避免[12][1]的情况误判
+            return st
+
+        return ser(t) in ser(s)
+
+
+# 作者：xu - yu - 23
+# 链接：https://leetcode.cn/problems/subtree-of-another-tree/solution/wu-xing-dai-ma-shi-jian-kong-jian-fen-bi-0gew/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+# 困难版：哈希树
+'''
+class Solution {
+    static final int MAX_N = 1005;
+    static final int MOD = 1000000007;
+    boolean[] vis = new boolean[MAX_N];
+    int[] p = new int[MAX_N];
+    int tot;
+    Map<TreeNode, int[]> hS = new HashMap<TreeNode, int[]>();
+    Map<TreeNode, int[]> hT = new HashMap<TreeNode, int[]>();
+
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        getPrime();
+        dfs(s, hS);
+        dfs(t, hT);
+
+        int tHash = hT.get(t)[0];
+        for (Map.Entry<TreeNode, int[]> entry : hS.entrySet()) {
+            if (entry.getValue()[0] == tHash) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void getPrime() {
+        vis[0] = vis[1] = true;
+        tot = 0;
+        for (int i = 2; i < MAX_N; ++i) {
+            if (!vis[i]) {
+                p[++tot] = i;
+            }
+            for (int j = 1; j <= tot && i * p[j] < MAX_N; ++j) {
+                vis[i * p[j]] = true;
+                if (i % p[j] == 0) {
+                    break;
+                }
+            }
+        }
+    }
+
+    public void dfs(TreeNode o, Map<TreeNode, int[]> h) {
+        h.put(o, new int[]{o.val, 1});
+        if (o.left == null && o.right == null) {
+            return;
+        }
+        if (o.left != null) {
+            dfs(o.left, h);
+            int[] val = h.get(o);
+            val[1] += h.get(o.left)[1];
+            val[0] = (int) ((val[0] + (31L * h.get(o.left)[0] * p[h.get(o.left)[1]]) % MOD) % MOD);
+        }
+        if (o.right != null) {
+            dfs(o.right, h);
+            int[] val = h.get(o);
+            val[1] += h.get(o.right)[1];
+            val[0] = (int) ((val[0] + (179L * h.get(o.right)[0] * p[h.get(o.right)[1]]) % MOD) % MOD);
+        }
+    }
+}
+
+作者：LeetCode-Solution
+链接：https://leetcode.cn/problems/subtree-of-another-tree/solution/ling-yi-ge-shu-de-zi-shu-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+'''
