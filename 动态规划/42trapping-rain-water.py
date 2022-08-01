65abf4eb3941ff216a916f43e614a002b477984e
wennroy <wennroy@gmail.com>
2022-08-01 01:36:31 -0500

    2022-08-01 经典动态规划题，还有单调栈和双指针解法未预习

---
+++ 动态规划/42trapping-rain-water.py
@@ -0,0 +1,34 @@
+# 超时 时间复杂度最差要O(n*m) m是值域最大值
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # val 值域作为dp
+        dp = [-1] * 10**5
+        ans = 0
+        for ind, num in enumerate(height):
+            for i in range(num):
+                if dp[i] != -1:
+                    ans += ind - dp[i] - 1
+                dp[i] = ind
+
+        return ans
+
+# 方法一：动态规划，寻找共同的覆盖面积
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        LeftMax, RightMax = [0] * n, [0] * n
+        LeftMax[-1] = height[-1]
+        RightMax[0] = height[0]
+        for i in range(n - 2, -1, -1):
+            LeftMax[i] = max(height[i], LeftMax[i + 1])
+        for i in range(1, n):
+            RightMax[i] = max(height[i], RightMax[i - 1])
+
+        ans = 0
+        for i in range(n):
+            ans += min(LeftMax[i], RightMax[i]) - height[i]
+
+        return ans
+
+# 方法二：单调栈
+
