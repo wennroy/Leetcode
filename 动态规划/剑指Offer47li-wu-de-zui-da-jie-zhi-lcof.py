ce1e1c2ef49e84566ea33a10849eaaac1ab1bc90
wennroy <wennroy@gmail.com>
2022-08-05 16:51:34 -0500

    2022-08-05 动态规划，只在原数据上操作即可

---
+++ 动态规划/剑指Offer47li-wu-de-zui-da-jie-zhi-lcof.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maxValue(self, grid: List[List[int]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+        m, n = len(grid), len(grid[0])
+        dp = [[0] * n for _ in range(m)]
+        dp[0][0] = grid[0][0]
+        for i in range(1, m):
+            dp[i][0] = dp[i - 1][0] + grid[i][0]
+
+        for j in range(1, n):
+            dp[0][j] = dp[0][j - 1] + grid[0][j]
+
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
+
+        return dp[-1][-1]
+
+# 改进后，只在grid上操作
+class Solution:
+    def maxValue(self, grid: List[List[int]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+        m, n = len(grid), len(grid[0])
+        for i in range(1, m):
+            grid[i][0] = grid[i - 1][0] + grid[i][0]
+
+        for j in range(1, n):
+            grid[0][j] = grid[0][j - 1] + grid[0][j]
+
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] = max(grid[i - 1][j], grid[i][j - 1]) + grid[i][j]
+
+        return grid[-1][-1]
