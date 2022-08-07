1d2e3178e2f1db713b414feab127bd6af3b1aa2d
wennroy <wennroy@gmail.com>
2022-08-07 00:06:51 -0500

    2022-08-06 周赛305 暴力记忆化搜索+动态规划

--- 周赛/Weekly-contest-305/6137check-if-there-is-a-valid-partition-for-the-array.py
+++ 周赛/Weekly-contest-305/6137check-if-there-is-a-valid-partition-for-the-array.py
@@ -19,3 +19,20 @@ class Solution:
             return False
 
         return validPart(0)
+
+# 更新一个DP动态规划算法，能记忆化搜索，看来就能动态规划，
+class Solution:
+    def validPartition(self, nums: List[int]) -> bool:
+        n = len(nums)
+        f = [True] + [False] * n
+        for i, x in enumerate(nums):
+            if i > 0 and f[i - 1] and x == nums[i - 1] or \
+               i > 1 and f[i - 2] and (x == nums[i - 1] == nums[i - 2] or
+                                       x == nums[i - 1] + 1 == nums[i - 2] + 2):
+               f[i + 1] = True
+        return f[n]
+
+# 作者：endlesscheng
+# 链接：https://leetcode.cn/problems/check-if-there-is-a-valid-partition-for-the-array/solution/by-endlesscheng-8y73/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
