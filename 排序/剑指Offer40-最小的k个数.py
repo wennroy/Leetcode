246c47aeac21d8f3689b2342aa2a62f8bdc41998
wennroy <wennroy@gmail.com>
2022-08-14 22:58:37 -0500

    2022-08-14 小根堆大根堆+快排

---
+++ 排序/剑指Offer40-最小的k个数.py
@@ -0,0 +1,59 @@
+import heapq
+import random
+
+class Solution:
+    def getLeastNumbers(self, arr: List[int], k: int) -> List[int]:
+        arr.sort()
+        return arr[:k]
+
+# 利用大根堆的特性，我们保持大根堆只有k个元素，要push的元素大于我们的最大值，则拒绝，最后留下的就是k小个数
+class Solution:
+    def getLeastNumbers(self, arr: List[int], k: int) -> List[int]:
+        if k == 0:
+            return list()
+
+        hp = [-x for x in arr[:k]]
+        heapq.heapify(hp)
+        for i in range(k, len(arr)):
+            if -hp[0] > arr[i]:
+                heapq.heappop(hp)
+                heapq.heappush(hp, -arr[i])
+        ans = [-x for x in hp]
+        return ans
+
+
+class Solution:
+    def partition(self, nums, l, r):
+        pivot = nums[r]
+        i = l - 1
+        for j in range(l, r):
+            if nums[j] <= pivot:
+                i += 1
+                nums[i], nums[j] = nums[j], nums[i]
+        nums[i + 1], nums[r] = nums[r], nums[i + 1]
+        return i + 1
+
+    def randomized_partition(self, nums, l, r):
+        i = random.randint(l, r)
+        nums[r], nums[i] = nums[i], nums[r]
+        return self.partition(nums, l, r)
+
+    def randomized_selected(self, arr, l, r, k):
+        pos = self.randomized_partition(arr, l, r)
+        num = pos - l + 1
+        if k < num:
+            self.randomized_selected(arr, l, pos - 1, k)
+        elif k > num:
+            self.randomized_selected(arr, pos + 1, r, k - num)
+
+    def getLeastNumbers(self, arr: List[int], k: int) -> List[int]:
+        if k == 0:
+            return list()
+        self.randomized_selected(arr, 0, len(arr) - 1, k)
+        return arr[:k]
+
+#
+# 作者：LeetCode-Solution
+# 链接：https://leetcode.cn/problems/zui-xiao-de-kge-shu-lcof/solution/zui-xiao-de-kge-shu-by-leetcode-solution/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
