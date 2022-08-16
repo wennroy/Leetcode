06a01e4b2426b732efe946372975e7531976bfbc
wennroy <wennroy@gmail.com>
2022-08-15 23:53:32 -0500

    2022-08-15 有限自动状态机，啥都不会，等着再看一遍

---
+++ 位运算/剑指Offer56-II数组中数字出现的次数II.py
@@ -0,0 +1,38 @@
+# 差生写法：
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        cnt = Counter(nums)
+        return cnt.most_common()[-1][0]
+
+# 有限状态自动机
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ones, twos = 0, 0
+        for num in nums:
+            ones = ones ^ num & ~twos
+            twos = twos ^ num & ~ones
+        return ones
+
+# 作者：jyd
+# 链接：https://leetcode.cn/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof/solution/mian-shi-ti-56-ii-shu-zu-zhong-shu-zi-chu-xian-d-4/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+# 遍历统计
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        counts = [0] * 32
+        for num in nums:
+            for j in range(32):
+                counts[j] += num & 1
+                num >>= 1
+        res, m = 0, 3
+        for i in range(32):
+            res <<= 1
+            res |= counts[31 - i] % m
+        return res if counts[31] % m == 0 else ~(res ^ 0xffffffff)
+
+# 作者：jyd
+# 链接：https://leetcode.cn/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof/solution/mian-shi-ti-56-ii-shu-zu-zhong-shu-zi-chu-xian-d-4/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
