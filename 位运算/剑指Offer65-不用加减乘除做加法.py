e09321da8695b0060a0a96ff0f470fe63e201d0b
wennroy <wennroy@gmail.com>
2022-08-15 23:08:15 -0500

    2022-08-15 位运算相关，净是些没见过的东西，有时间可以复习一下

---
+++ 位运算/剑指Offer65-不用加减乘除做加法.py
@@ -0,0 +1,12 @@
+class Solution:
+    def add(self, a: int, b: int) -> int:
+        x = 0xffffffff
+        a, b = a & x, b & x
+        while b != 0:
+            a, b = (a ^ b), (a & b) << 1 & x
+        return a if a <= 0x7fffffff else ~(a ^ x)
+
+# 作者：jyd
+# 链接：https://leetcode.cn/problems/bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof/solution/mian-shi-ti-65-bu-yong-jia-jian-cheng-chu-zuo-ji-7/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
