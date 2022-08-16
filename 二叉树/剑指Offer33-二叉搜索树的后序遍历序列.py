700f6bf9ca6a0caaeeb5a62993585fdd54d5fb91
wennroy <wennroy@gmail.com>
2022-08-15 21:39:04 -0500

    2022-08-15 二叉搜索树的后序遍历序列。递归分治+单调栈

---
+++ 二叉树/剑指Offer33-二叉搜索树的后序遍历序列.py
@@ -0,0 +1,56 @@
+# 二叉搜索树的后续遍历
+# 自己写的递归分治法
+
+class Solution:
+    def verifyPostorder(self, postorder: List[int]) -> bool:
+        n = len(postorder)
+        if n <= 2:
+            return True
+
+        root_pt = postorder[-1]
+
+        for i, pt in enumerate(postorder):
+            if pt > root_pt:
+                break
+
+        for j, pt in enumerate(postorder[i:]):
+            if pt < root_pt:
+                return False
+
+        return self.verifyPostorder(postorder[:i]) and self.verifyPostorder(postorder[i:-1])
+
+# 递归分治
+
+class Solution:
+    def verifyPostorder(self, postorder: [int]) -> bool:
+        def recur(i, j):
+            if i >= j: return True
+            p = i
+            while postorder[p] < postorder[j]: p += 1
+            m = p
+            while postorder[p] > postorder[j]: p += 1
+            return p == j and recur(i, m - 1) and recur(m, j - 1)
+
+        return recur(0, len(postorder) - 1)
+
+# 作者：jyd
+# 链接：https://leetcode.cn/problems/er-cha-sou-suo-shu-de-hou-xu-bian-li-xu-lie-lcof/solution/mian-shi-ti-33-er-cha-sou-suo-shu-de-hou-xu-bian-6/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+# 单调栈
+
+class Solution:
+    def verifyPostorder(self, postorder: [int]) -> bool:
+        stack, root = [], float("+inf")
+        for i in range(len(postorder) - 1, -1, -1):
+            if postorder[i] > root: return False
+            while(stack and postorder[i] < stack[-1]):
+                root = stack.pop()
+            stack.append(postorder[i])
+        return True
+
+# 作者：jyd
+# 链接：https://leetcode.cn/problems/er-cha-sou-suo-shu-de-hou-xu-bian-li-xu-lie-lcof/solution/mian-shi-ti-33-er-cha-sou-suo-shu-de-hou-xu-bian-6/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
