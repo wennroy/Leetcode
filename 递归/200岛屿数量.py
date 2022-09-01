602f5c17d482d39b43a98744fb7249b2476d86ca
wennroy <wennroy@gmail.com>
2022-08-31 19:53:57 -0500

    2022-08-31 DFS BFS，还能用并查集

---
+++ 递归/200岛屿数量.py
@@ -0,0 +1,84 @@
+# 直接遍历DFS啥的即可
+DIR = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m, n = len(grid), len(grid[0])
+        visited = [[False] * n for _ in range(m)]
+
+        def recur(i, j):
+            visited[i][j] = True
+            for x, y in DIR:
+                new_x = x + i
+                new_y = y + j
+                if 0 <= new_x < m and 0 <= new_y < n and not visited[new_x][new_y] and grid[new_x][new_y] == '1':
+                    recur(new_x, new_y)
+
+        ans = 0
+        for i in range(m):
+            for j in range(n):
+                if visited[i][j] == False and grid[i][j] == '1':
+                    recur(i, j)
+                    ans += 1
+
+        return ans
+
+
+# 还能用并查集
+class UnionFind:
+    def __init__(self, grid):
+        m, n = len(grid), len(grid[0])
+        self.count = 0
+        self.parent = [-1] * (m * n)
+        self.rank = [0] * (m * n)
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    self.parent[i * n + j] = i * n + j
+                    self.count += 1
+
+    def find(self, i):
+        if self.parent[i] != i:
+            self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+
+    def union(self, x, y):
+        rootx = self.find(x)
+        rooty = self.find(y)
+        if rootx != rooty:
+            if self.rank[rootx] < self.rank[rooty]:
+                rootx, rooty = rooty, rootx
+            self.parent[rooty] = rootx
+            if self.rank[rootx] == self.rank[rooty]:
+                self.rank[rootx] += 1
+            self.count -= 1
+
+    def getCount(self):
+        return self.count
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        nr = len(grid)
+        if nr == 0:
+            return 0
+        nc = len(grid[0])
+
+        uf = UnionFind(grid)
+        num_islands = 0
+        for r in range(nr):
+            for c in range(nc):
+                if grid[r][c] == "1":
+                    grid[r][c] = "0"
+                    for x, y in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
+                        if 0 <= x < nr and 0 <= y < nc and grid[x][y] == "1":
+                            uf.union(r * nc + c, x * nc + y)
+
+        return uf.getCount()
+
+
+# 作者：LeetCode
+# 链接：https: // leetcode.cn / problems / number - of - islands / solution / dao - yu - shu - liang - by - leetcode /
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
