f9d55953cc7bf13cab8eeb38e413756572255848
wennroy <wennroy@gmail.com>
2022-09-06 17:16:44 -0500

    2022-09-06 更新，统计连续字符串的子串，我们可以改变角度，考虑这个字符被多少个字符串包含。

--- 字符串/828统计子串中的唯一字符.py
+++ 字符串/828统计子串中的唯一字符.py
@@ -1,5 +1,8 @@
 # 没看懂，等会儿再看看
 
+# res += (arr[i] - arr[i - 1]) * (arr[i + 1] - arr[i])
+# 从字符串的起始位置 (arr[i - 1], arr[i]] 到哦字符串的终止位置 [arr[i], arr[i+1]) 为止，总共有
+# (arr[i] - arr[i - 1]) * (arr[i + 1] - arr[i])种组合包含这个字符。
 import collections
 class Solution:
     def uniqueLetterString(self, s: str) -> int:
