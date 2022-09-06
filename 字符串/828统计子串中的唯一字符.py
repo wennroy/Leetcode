200c78da080c1c73973134e1587da8dc4eb85129
wennroy <wennroy@gmail.com>
2022-09-06 16:31:59 -0500

    2022-09-06 计算唯一的子字符串。

---
+++ 字符串/828统计子串中的唯一字符.py
@@ -0,0 +1,17 @@
+# 没看懂，等会儿再看看
+
+import collections
+class Solution:
+    def uniqueLetterString(self, s: str) -> int:
+        index = collections.defaultdict(list)
+        for i, c in enumerate(s):
+            index[c].append(i)
+
+        print(index)
+
+        res = 0
+        for arr in index.values():
+            arr = [-1] + arr + [len(s)]
+            for i in range(1, len(arr) - 1):
+                res += (arr[i] - arr[i - 1]) * (arr[i + 1] - arr[i])
+        return res
