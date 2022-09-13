8c1c102f14919edcf91a599148f38c1599756611
wennroy <wennroy@gmail.com>
2022-09-13 18:53:14 -0500

    2022-09-13 更新：位运算解法，本质思路为BFS，但时间快很多。经典动态规划问题

--- 动态规划/322零钱兑换.py
+++ 动态规划/322零钱兑换.py
@@ -77,3 +77,38 @@ class Solution:
 # 链接：https: // leetcode.cn / problems / coin - change / solution / 322 - ling - qian - dui - huan - by - leetcode - solution /
 # 来源：力扣（LeetCode）
 # 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+# 位运算
+# 利用位运算来模拟能达到的最远距离，一旦最后一位是1，则达到此处。
+# 本质是一种BFS的过程
+
+class Solution:
+    def coinChange(self, coins, amount: int) -> int:
+        if not amount:
+            return 0
+        # 将减法转换为除法进行运算
+        # 一旦最低位为1，则说明找到解，停止运算
+        dp = 1 << amount
+        res = 0
+        while dp:
+            tmp = 0
+            res += 1
+            # 每一轮运算计算一遍dp除以2**i得到的所有可能解
+            for coin in coins:
+                # tmp用于存储运算的中间结果
+                # dp >> coin 实际上是进行除法运算：dp//2**coin
+                # 使用位运算“或”来保存全部除法运算结果中的‘1’，实现批量运算
+                # ps:这也是二进制移位的一个神奇之处，大家可以手动模拟一下这个过程
+                # tmp有点类似于能达到的最远距离
+                tmp |= dp >> coin
+            # 一旦末尾出现1，则返回结果
+            if tmp & 1:
+                return res
+            # 将本轮运算的全部运算结果送入下一轮计算
+            dp = tmp
+        return -1
+
+# 作者：CodeHard_LiveFun
+# 链接：https://leetcode.cn/problems/coin-change/solution/by-codehard_livefun-uzn9/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
